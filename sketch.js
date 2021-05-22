47d2bd57503ff1863313243eaa83abf45f1804ee
Champions_017 <soham.chauhan@jnis.ac.in>
2021-05-22 13:51:16 +0530

    created a new log

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var log8
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -36,6 +37,7 @@ function setup(){
 
     bird = new Bird(100,100);
 
+    log8 = new Log(100,100,150,PI/2)
 }
 
 function draw(){
@@ -58,6 +60,8 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+
+    log8.display();
 
     bird.display();
     platform.display();
